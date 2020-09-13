102d2c61754a3bb2678f74baae8d690247986e6b
Azizur <azizur.rahman363410@gmail.com>
2020-09-13 10:59:44 +0600

    First Inventory Work

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Meta Inventory Work',
+    'version': '1.1.1',
+    'category': 'Inventory',
+    'sequence': 1,
+    'summary': 'Inventory Adjustment Chassis Number Engine Number',
+    'description': "",
+    'website': 'https://metamorphosis.com.bd/',
+    'author': 'Metamorphosis',
+    'depends': ['stock'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
